5923780ee59738ca035e0d69235ac34c6d58cca1
Sam <samuel.house@ntlworld.com>
2017-07-02 22:01:47 +0100

    moved meta links to index.ejs and removed helmet

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
-import { Helmet } from "react-helmet";
 
 export default class Layout extends Component {
   render() {
     return (
       <div className="wrap">
-        <Helmet>
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css" />
-          <link href="https://fonts.googleapis.com/css?family=Lato|Philosopher" rel="stylesheet" />
-        </Helmet>
         <Navbar inverse collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
